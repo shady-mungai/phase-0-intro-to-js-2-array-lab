30e82d2d8c3a44d14837c4758dd9b9fb25c3d308
shady-mungai <shadrackmungai10@gmail.com>
2022-12-05 12:00:29 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
 // Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"];
+function cats(cats){
+  return cats
+}
+// cats.push['Milo', 'Otis', 'Garfield'];
+
+var name = "Ralph";
+function destructivelyAppendCat(name)
+{
+     cats.push(name);
+     console.log(cats)
+     return cats;
+}
+
+var name="Bob";
+function destructivelyPrependCat(name)
+{
+     cats.unshift(name);
+     return cats;
+}
+
+function destructivelyRemoveLastCat()
+{
+    cats.pop();
+    return cats;
+}
+
+function  destructivelyRemoveFirstCat()
+{
+    cats.shift();
+    return cats;
+}
+
+var name="Broom";
+function appendCat(name)
+{
+    var cats1 = [...cats];
+    cats1.push(name);
+    return cats1;
+}
+
+var name="Arnold";
+function prependCat(name)
+{
+    var cats1 = [...cats];
+    cats1.unshift(name);
+    return cats1;
+}
+
+
+function removeLastCat()
+{
+    var cats1 = [...cats];
+    cats1.pop();
+    return cats1;
+}
+
+
+function removeFirstCat()
+{
+    var cats1 = [...cats];
+    cats1.shift();
+    return cats1;
+}
